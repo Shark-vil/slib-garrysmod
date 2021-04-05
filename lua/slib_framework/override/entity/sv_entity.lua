b391d79943ee92afab69aae141804db8b7363ba0
Shark-vil <shark_vil@mail.ru>
2021-04-05 18:54:47 +0300

    add entity rpc callback function, change firstspawn hook

--- lua/slib_framework/override/entity/sv_entity.lua
+++ lua/slib_framework/override/entity/sv_entity.lua
@@ -9,3 +9,32 @@ hook.Add("SlibPlayerFirstSpawn", "Slib_SyncExistsNetworkVariable", function(ply)
       end
    end
 end)
+
+snet.RegisterCallback('snet_entity_tool_call_server_rpc', function(ply, ent, tool_mode, func_name, ...)
+   if not ent or not IsValid(ent) or ent:GetClass() ~= 'gmod_tool' then return end
+
+   local owner = ent:GetOwner()
+   if IsValid(owner) and owner:IsPlayer() and owner ~= ply then
+      return
+   end
+
+   local tool = ply:GetTool()
+   if tool:GetMode() ~= tool_mode then return end
+   
+   local func = tool[func_name]
+   if not func then return end
+   func(tool, ...)
+end)
+
+snet.RegisterCallback('snet_entity_call_server_rpc', function(ply, ent, func_name, ...)
+   if not ent or not IsValid(ent) then return end
+
+   local owner = ent:GetOwner()
+   if IsValid(owner) and owner:IsPlayer() and owner ~= ply then
+      return
+   end
+   
+   local func = ent[func_name]
+   if not func then return end
+   func(ent, ...)
+end)
