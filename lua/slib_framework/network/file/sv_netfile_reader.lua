291a9e9adbe015ad59dedfe51b622d7bc8540746
Shark-vil <shark_vil@mail.ru>
2021-04-26 06:37:54 +0300

    Added a method for remotely checking the existence of a file.

--- lua/slib_framework/network/file/sv_netfile_reader.lua
+++ lua/slib_framework/network/file/sv_netfile_reader.lua
@@ -1,32 +1,65 @@
 local wait_read_data = {}
+local wait_exist_check = {}
 
 snet.Callback('snet_file_read_in_client_get', function(ply, customId, data)
-   for i = #wait_read_data, 1, -1 do
-      local value = wait_read_data[i]
-      if value.id == customId then
-         value.func(data)
-         table.remove(wait_read_data, i)
-         break
-      end
-   end
+	for i = #wait_read_data, 1, -1 do
+		local value = wait_read_data[i]
+
+		if value.id == customId then
+			value.func(data)
+			table.remove(wait_read_data, i)
+			break
+		end
+	end
 end).Protect().Register()
 
 snet.Callback('snet_file_read_in_server', function(ply, path, customId)
-   local data = slib.FileRead(path)
-   snet.Invoke('snet_file_read_in_server_get', ply, customId, data)
+	local data = slib.FileRead(path)
+	snet.Invoke('snet_file_read_in_server_get', ply, customId, data)
 end).Protect().Register()
 
-function snet.FileReadInClient(ply, path, onRead)
-   local name = 'snet_file_read_in_client'
-   local customId = slib.GenerateUid(name)
+function snet.FileReadInClient(ply, path, response_action)
+	local name = 'snet_file_read_in_client'
+	local customId = slib.GenerateUid(name)
 
-   local request = snet.Create(name, path, customId)
-   request.id = customId
+	local request = snet.Create(name, path, customId)
+	request.id = customId
 
-   table.insert(wait_read_data, {
-      id = request.id,
-      func = onRead
-   })
+	table.insert(wait_read_data, {
+		id = request.id,
+		func = response_action
+	})
 
-   request.Invoke(ply)
+	request.Invoke(ply)
+end
+
+snet.Callback('snet_file_exists_in_client_get', function(ply, customId, is_exist)
+	for i = #wait_exist_check, 1, -1 do
+		local value = wait_exist_check[i]
+
+		if value.id == customId then
+			value.func(is_exist)
+			table.remove(wait_exist_check, i)
+			break
+		end
+	end
+end).Protect().Register()
+
+snet.Callback('snet_file_exists_in_server', function(ply, path, customId)
+	snet.Invoke('snet_file_exists_in_server_get', ply, customId, slib.FileExists(path))
+end).Protect().Register()
+
+function snet.FileExistsInClient(ply, path, response_action)
+	local name = 'snet_file_exists_in_client'
+	local customId = slib.GenerateUid(name)
+
+	local request = snet.Create(name, path, customId)
+	request.id = customId
+
+	table.insert(wait_exist_check, {
+		id = request.id,
+		func = response_action
+	})
+
+	request.Invoke(ply)
 end
