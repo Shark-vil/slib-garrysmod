431974069955de566afbcaaa1c00d44e164b1126
Shark-vil <shark_vil@mail.ru>
2021-04-26 05:53:04 +0300

    Added an event listener for the GUI

---
+++ lua/slib_framework/extension/gui/cl_listener.lua
@@ -0,0 +1,33 @@
+local function ExecuteListener(panel, action_name, ...)
+	local listeners = panel.sgui_listeners[action_name]
+	for _, f in ipairs(listeners) do
+		f(...)
+	end
+end
+
+function sgui.SystemParentListener(PANEL)
+	if PANEL.sgui_listeners then return end
+
+	PANEL.sgui_listeners_enabled = {}
+	PANEL.sgui_listeners = {}
+
+	function PANEL:AddListener(action_name, func)
+		self.sgui_listeners[action_name] = self.sgui_listeners[action_name] or {}
+
+		table.insert(self.sgui_listeners[action_name], func)
+
+		if not self.sgui_listeners_enabled[action_name] then
+			self[action_name] = function(...)
+				ExecuteListener(self, action_name, ...)
+			end
+			self.sgui_listeners_enabled[action_name] = true
+		end
+	end
+
+	function PANEL:ClearListeners(action_name)
+		if not self.sgui_listeners_enabled[action_name] then return end
+		self.sgui_listeners_enabled[action_name] = false
+		self.sgui_listeners = {}
+		self[action_name] = nil
+	end
+end
