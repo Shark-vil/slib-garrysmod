4421cca35c9a50043f07394de890808d2206d79f
Shark-vil <shark_vil@mail.ru>
2021-05-04 03:12:38 +0300

    change uid generator function

--- lua/slib_framework/extension/sh_generators.lua
+++ lua/slib_framework/extension/sh_generators.lua
@@ -1,7 +1,12 @@
 local uid = 0
+
+function slib.GetUid()
+	uid = uid + 1
+	return uid
+end
+
 function slib.GenerateUid(salt)
-   salt = salt or ''
-   if not isstring(salt) then salt = tostring(salt) end
-   uid = uid + 1
-   return tostring(util.CRC(salt .. CurTime() .. uid))
+	salt = salt or ''
+	if not isstring(salt) then salt = tostring(salt) end
+	return tostring(util.CRC(salt .. slib.GetUid()))
 end
