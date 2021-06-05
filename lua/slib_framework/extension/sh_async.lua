9669e6b6fd77c3ef4358fbc4b73b5940aeb4dedc
Shark-vil <shark_vil@mail.ru>
2021-06-05 18:30:02 +0300

    Recycle capability for asynchronous function

--- lua/slib_framework/extension/sh_async.lua
+++ lua/slib_framework/extension/sh_async.lua
@@ -1,13 +1,21 @@
 async = async or {}
 
+local yield = coroutine.yield
+local wait = coroutine.wait
+
 function async.Add(id, func)
    async.Remove(id)
 
-   local co
+   local co, worked, is_repeat
+
    hook.Add('Think', 'slib_async_' .. id, function()
-      if not co or not coroutine.resume(co, coroutine.yield, coroutine.wait) then
-         co = coroutine.create(func)
-         coroutine.resume(co, coroutine.yield, coroutine.wait)
+      if not co or not worked then co = coroutine.create(func) end
+
+      worked, is_repeat = coroutine.resume(co, yield, wait)
+
+      if not worked and not is_repeat then
+         async.Remove(id)
+         return
       end
    end)
 end
