88b20e92ce78b0055edfd242ccc7f3e89ce01368
Shark-vil <shark_vil@mail.ru>
2021-06-13 19:54:45 +0300

    change var name type to notify_type

--- lua/slib_framework/override/player/sh_player.lua
+++ lua/slib_framework/override/player/sh_player.lua
@@ -1,16 +1,16 @@
 local meta = FindMetaTable('Player')
 
-function meta:slibNotify(text, type, length, sound)
+function meta:slibNotify(text, notify_type, length, sound)
    text = text or ''
-   type = type or NOTIFY_GENERIC
+   notify_type = notify_type or NOTIFY_GENERIC
    length = length or 3
 
    if SERVER then
-      snet.Invoke('slib_player_notify', self, text, type, length, sound)
+      snet.Invoke('slib_player_notify', self, text, notify_type, length, sound)
       return
    end
 
-   notification.AddLegacy(text, type, length)
+   notification.AddLegacy(text, notify_type, length)
    if sound then surface.PlaySound(sound) end
 end
 
