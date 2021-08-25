1e034192ee09a9e8be7ad25cf5e24f76f97f7f34
Shark-vil <shark_vil@mail.ru>
2021-08-26 01:15:38 +0300

    Added router module for GUI

---
+++ lua/slib_framework/extension/gui/sh_routes.lua
@@ -0,0 +1,28 @@
+sgui.routes_storage = sgui.routes_storage or {}
+
+if CLIENT then
+	function sgui.RouteRegister(adr, func)
+		if not isstring(adr) or not isfunction(func) then return end
+		sgui.routes_storage[adr] = func
+	end
+end
+
+function sgui.route(adr, ...)
+	if SERVER then
+		local args = { ... }
+		local first_arg = args[1]
+		if not first_arg or not isentity(first_arg) or not first_arg:IsPlayer() then return end
+		table.remove(args, 1)
+		snet.Invoke('sgui_route_open_by_server', first_arg, unpack(args))
+		return
+	end
+
+	if not sgui.routes_storage[adr] then return end
+	return sgui.routes_storage[adr](...)
+end
+
+if CLIENT then
+	snet.Callback('sgui_route_open_by_server', function(_, adr, ...)
+		sgui.route(adr, ...)
+	end)
+end
