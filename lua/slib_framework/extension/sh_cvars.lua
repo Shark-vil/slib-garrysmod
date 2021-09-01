4907a85dee01bee321ab0e939385bf5686a9e257
Shark-vil <shark_vil@mail.ru>
2021-09-01 23:27:11 +0300

    Function for checking the value of the cvar

---
+++ lua/slib_framework/extension/sh_cvars.lua
@@ -0,0 +1,7 @@
+function slib.CvarCheckValue(cvar_name, check_value)
+	local convar_object = GetConVar(cvar_name)
+	if not convar_object then return false end
+	local value = convar_object:GetString()
+	local convert_value = tostring(check_value)
+	return convert_value == value
+end
