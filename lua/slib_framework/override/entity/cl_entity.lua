f88bc6eccbd1c13210bc576dd7fcaa18c06bf12e
Shark-vil <shark_vil@mail.ru>
2021-09-01 23:29:56 +0300

    Fixed formatting and added log

--- lua/slib_framework/override/entity/cl_entity.lua
+++ lua/slib_framework/override/entity/cl_entity.lua
@@ -1,13 +1,14 @@
 snet.Callback('slib_entity_variable_set', function (_, ent, key, value)
+	slib.Log('Set ', ent, ' variable [ ', key, ' : ', value, ' ]')
 	ent:slibSetVar(key, value)
-end).Validator(SNET_ENTITY_VALIDATOR).Register()
+end).Validator(SNET_ENTITY_VALIDATOR)
 
 snet.Callback('slib_entity_variable_del', function (_, ent, key)
+	slib.Log('Delete ', ent, ' variable [ ', key, ' ]')
 	ent:slibSetVar(key, nil)
-end).Validator(SNET_ENTITY_VALIDATOR).Register()
+end).Validator(SNET_ENTITY_VALIDATOR)
 
-snet.RegisterCallback('snet_entity_tool_call_client_rpc',
-function(ply, ent, tool_mode, func_name, ...)
+snet.RegisterCallback('snet_entity_tool_call_client_rpc', function(ply, ent, tool_mode, func_name, ...)
 	if not ent or not IsValid(ent) or ent:GetClass() ~= 'gmod_tool' then return end
 
 	local tool = ply:GetTool()
@@ -19,12 +20,11 @@ function(ply, ent, tool_mode, func_name, ...)
 	func(tool, ...)
 end)
 
-snet.Callback('snet_entity_call_client_rpc',
-function(ply, ent, func_name, ...)
+snet.Callback('snet_entity_call_client_rpc', function(ply, ent, func_name, ...)
 	if not ent or not IsValid(ent) then return end
 
 	local func = ent[func_name]
 	if not func then return end
 
 	func(ent, ...)
-end).Validator(SNET_ENTITY_VALIDATOR).Register()
+end).Validator(SNET_ENTITY_VALIDATOR)
