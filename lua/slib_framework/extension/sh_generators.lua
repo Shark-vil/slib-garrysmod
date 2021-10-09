70a861616084dd9f90e720fb47a4d69dc55a0dfb
Shark-vil <shark_vil@mail.ru>
2021-10-09 16:15:39 +0300

    Added UUID string generator. Upgrade version.

--- lua/slib_framework/extension/sh_generators.lua
+++ lua/slib_framework/extension/sh_generators.lua
@@ -1,6 +1,10 @@
 local tostring = tostring
 local util_CRC = util.CRC
 local util_Base64Encode = util.Base64Encode
+local math_random = math.random
+local uuid_template = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'
+local string_gsub = string.gsub
+local string_format = string.format
 --
 
 local uid = 0
@@ -21,4 +25,13 @@ function slib.GenerateUidHash( salt )
 	salt = salt or ''
 	uid_hash = uid_hash + 1
 	return tostring( util_Base64Encode( tostring( salt ) .. tostring( uid_hash ) ) )
+end
+
+-- Source:
+-- https://gist.github.com/jrus/3197011
+function slib.UUID()
+	return string_gsub(uuid_template, '[xy]', function (c)
+		local v = (c == 'x') and math_random(0, 0xf) or math_random(8, 0xb)
+		return string_format('%x', v)
+	end)
 end
