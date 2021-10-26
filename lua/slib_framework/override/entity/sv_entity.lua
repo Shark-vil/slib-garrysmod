82e803a55d5efcfe08efd513c0c48d765d68fa93
Shark-vil <shark_vil@mail.ru>
2021-10-27 02:33:44 +0300

    add slibFixPVS

--- lua/slib_framework/override/entity/sv_entity.lua
+++ lua/slib_framework/override/entity/sv_entity.lua
@@ -1,6 +1,8 @@
 local snet = slib.Components.Network
 local IsValid = IsValid
 --
+local meta = FindMetaTable('Entity')
+local pvs_entities = {}
 
 snet.RegisterCallback('snet_entity_tool_call_server_rpc', function(ply, ent, tool_mode, func_name, ...)
 	if not ent or not IsValid(ent) or ent:GetClass() ~= 'gmod_tool' then return end
@@ -27,4 +29,25 @@ snet.RegisterCallback('snet_entity_call_server_rpc', function(ply, ent, func_nam
 	if not func then return end
 
 	func(ent, ...)
+end)
+
+function meta:slibFixPVS()
+	table.insert(pvs_entities, {
+		ent = self,
+		reset = CurTime() + 2
+	})
+end
+
+hook.Add('SetupPlayerVisibility', 'SlibEntityOverrideFixPVS', function()
+	for i = #pvs_entities, 1, -1 do
+		local item = pvs_entities[i]
+		local ent = item.ent
+		local reset = item.reset
+
+		if not ent or not IsValid(ent) or reset < CurTime() then
+			table.remove(pvs_entities, i)
+		else
+			AddOriginToPVS(ent:GetPos())
+		end
+	end
 end)
