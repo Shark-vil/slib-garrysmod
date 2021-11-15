927f2c7b28dbddc244fa533e8a1652c19003fa5e
Shark-vil <shark_vil@mail.ru>
2021-11-15 20:31:57 +0300

    Update animator register. Added settings options.

--- lua/slib_framework/animator/sh_animator.lua
+++ lua/slib_framework/animator/sh_animator.lua
@@ -1,11 +1,10 @@
-function slib.Animator.RegisterAnimation(name, sequence, model)
+function slib.Animator.RegisterAnimation(name, model)
 	local i, _ = table.WhereFindBySeq(slib.Storage.Animations, function(_, v) return v.name == name end)
 	if i ~= -1 then table.remove(slib.Storage.Animations, i) end
 
 	table.insert(slib.Storage.Animations, {
 		name = name,
 		model = Model(model),
-		sequence = sequence
 	})
 end
 
