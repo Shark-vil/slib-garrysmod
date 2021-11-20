702e1ef959c1767ddcf963c0d2acef5e99dd411e
Shark-vil <shark_vil@mail.ru>
2021-11-21 01:10:10 +0300

    Upgrade animator service

--- lua/slib_framework/animator/sv_animator_services.lua
+++ lua/slib_framework/animator/sv_animator_services.lua
@@ -15,15 +15,16 @@ hook.Add('Move', 'SlibOverrideAnimatorPlayerMovement', function(ply, mv)
 		local settings = value.settings
 		local entity = value.entity
 		local is_player = value.is_player
+		local stop_time = value.stop_time
 
 		if not is_player or not IsValid(entity) or entity ~= ply or settings.not_lock_movement then
 			continue
 		end
 
 		for k = 1, #movement_keys do
-			if mv:KeyDown(movement_keys[k]) then
+			if mv:KeyDown(movement_keys[k]) and not settings.loop and stop_time > CurTime() + .1 then
 				ply:Freeze(true)
-				timer.Simple(.5, function() ply:Freeze(false) end)
+				timer.Simple(.3, function() ply:Freeze(false) end)
 				return true
 			end
 		end
@@ -38,16 +39,24 @@ local function GetAnimationInfo(ent)
 end
 
 local function DestroyAnimatorAction(ent)
-	if not GetAnimationInfo(ent) then return end
+	local anim = GetAnimationInfo(ent)
+	if not anim then return end
+
+	if isfunction(anim.OnDestroy) then anim.OnDestroy(ent) end
+
 	snet.InvokeAll('slib_animator_destroyed', ent)
 	slib.Animator.ClearInactive()
 end
 hook.Add('EntityRemoved', 'SlibAnimatorRemoveIfEntityDestroyed', DestroyAnimatorAction)
 hook.Add('OnNPCKilled', 'SlibAnimatorRemoveIfEntityDestroyed', DestroyAnimatorAction)
 hook.Add('PlayerDeath', 'SlibAnimatorRemoveIfEntityDestroyed', DestroyAnimatorAction)
-hook.Add('EntityTakeDamage', 'SlibAnimatorRemoveIfEntityTakeDamage', function(ent)
+hook.Add('EntityTakeDamage', 'SlibAnimatorRemoveIfEntityTakeDamage', function(ent, dmginfo)
 	local anim = GetAnimationInfo(ent)
-	if not anim or not IsValid(anim.animator) or not anim.settings.stop_on_damage then return end
+	if not anim or not IsValid(anim.animator) then return end
+
+	if isfunction(anim.OnTakeDamage) then anim.OnTakeDamage(ent, dmginfo) end
+	if not anim.settings.stop_on_damage then return end
+
 	anim.animator:Remove()
 	snet.InvokeAll('slib_animator_destroyed', ent)
 	slib.Animator.ClearInactive()
