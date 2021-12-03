1c9cc6f4ffa1b9857a53f0d3ca0dd917592d9a58
Shark-vil <shark_vil@mail.ru>
2021-12-03 20:57:06 +0300

    https://github.com/Shark-vil/slib-garrysmod/issues/45

--- lua/slib_framework/extension/gui/cl_default_listeners.lua
+++ lua/slib_framework/extension/gui/cl_default_listeners.lua
@@ -4,6 +4,7 @@ local isfunction = isfunction
 
 function sgui.SystemParentDefaultListeners(PANEL)
 	PANEL:AddListener('Think', function(self)
+		if not self then return end
 		if not self.OnChangeHovered or not isfunction(self.OnChangeHovered) then return end
 
 		local is_hovered = self:IsHovered()
@@ -17,21 +18,25 @@ function sgui.SystemParentDefaultListeners(PANEL)
 	end)
 
 	PANEL:AddListener('DoClick', function(self)
+		if not self then return end
 		if not self.sgui_sound_click or not isstring(self.sgui_sound_click) then return end
 		surface.PlaySound(self.sgui_sound_click)
 	end)
 
 	PANEL:AddListener('OnChangeHovered', function(self, _, is_hovered)
+		if not self then return end
 		if not self.sgui_sound_switch_hovered or not isstring(self.sgui_sound_switch_hovered) then return end
 		if not is_hovered then return end
 		surface.PlaySound(self.sgui_sound_switch_hovered)
 	end)
 
 	PANEL:AddListener('OnDepressed', function(self)
+		if not self then return end
 		self.sgui_is_pressed = true
 	end)
 
 	PANEL:AddListener('OnReleased', function(self)
+		if not self then return end
 		self.sgui_is_pressed = false
 	end)
 end
