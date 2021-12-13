3e5f0b0f26171963f6ca29eec8a5da4fadff2d34
Shark-vil <shark_vil@mail.ru>
2021-12-13 05:23:48 +0300

    Enumeration of languages

--- lua/slib_framework/extension/sh_helpers.lua
+++ lua/slib_framework/extension/sh_helpers.lua
@@ -1,14 +1,70 @@
 local engine_TickInterval = engine.TickInterval
 local math_sqrt = math.sqrt
+--
+local language_codes = {
+	['bg'] = 'bulgarian',
+	['cs'] = 'czech',
+	['da'] = 'danish',
+	['de'] = 'german',
+	['el'] = 'greek',
+	['en'] = 'english',
+	['en-pt'] = 'pirate english',
+	['es-es'] = 'spanish',
+	['et'] = 'estonian',
+	['fi'] = 'finnish',
+	['fr'] = 'french',
+	['he'] = 'hebrew',
+	['hr'] = 'croatian',
+	['hu'] = 'hungarian',
+	['it'] = 'italian',
+	['ja'] = 'japanese',
+	['ko'] = 'korean',
+	['lt'] = 'lithuanian',
+	['nl'] = 'dutch',
+	['no'] = 'norwegian',
+	['pl'] = 'polish',
+	['pt-br'] = 'portuguese (brazil)',
+	['pt-pt'] = 'portuguese (portugal)',
+	['ru'] = 'russian',
+	['sk'] = 'slovak',
+	['sv-se'] = 'swedish',
+	['th'] = 'thai',
+	['tr'] = 'turkish',
+	['uk'] = 'ukrainian',
+	['vi'] = 'vietnamese',
+	['zh-cn'] = 'chinese simplified',
+	['zh-tw'] = 'chinese traditional',
+}
+
+function slib.GetLanguageCode(select_language)
+	local _select_language = tostring(select_language):lower()
+	for code, value in pairs(language_codes) do
+		if value == _select_language or code == _select_language then return code, value end
+	end
+end
 
 function slib.language(data, select_language)
 	if not istable(data) then return '' end
-	if select_language and data[select_language] then return data[select_language] end
+
+	if select_language and isstring(select_language) then
+		if select_language and data[select_language] then return data[select_language] end
+
+		local code, lang = slib.GetLanguageCode(select_language)
+		if code and data[code] then return data[code] end
+		if lang and data[lang] then return data[lang] end
+	end
+
 	if CLIENT then
 		local current_language = GetConVar('cl_language'):GetString()
 		if data[current_language] then return data[current_language] end
+
+		local code, lang = slib.GetLanguageCode(current_language)
+		if code and data[code] then return data[code] end
+		if lang and data[lang] then return data[lang] end
 	end
+
 	if data['default'] then return data['default'] end
+
 	return ''
 end
 
