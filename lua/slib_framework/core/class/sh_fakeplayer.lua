6048307b9361fca602fc691d3c376bf3108a9c41
Shark-vil <shark_vil@mail.ru>
2022-07-08 06:51:01 +0300

    Move sh_fakeplayer

---
+++ lua/slib_framework/core/class/sh_fakeplayer.lua
@@ -0,0 +1,29 @@
+-- Copyright © 2018-2030 Decent Vehicle
+-- written by ∩(≡＾ω＾≡)∩ (https://steamcommunity.com/id/greatzenkakuman/)
+-- and DangerKiddy(DK) (https://steamcommunity.com/profiles/76561198132964487/).
+-- Modification - Shark_vil
+
+local Class = {}
+local uid = 0
+
+function Class:Make(name, pos, ang)
+	name = name or 'FakePlayer' .. uid
+	pos = pos or Vector(0, 0, 0)
+	ang = ang or Angle(0, 0, 0)
+	uid = uid + 1
+
+	local ent = ents.Create('slib_fakeplayer')
+	ent.PrintName = name
+	ent:SetPos(pos)
+	ent:SetAngles(ang)
+
+	return ent
+end
+
+function Class:Spawn(name, pos, ang)
+	local ent = self:Make(name, pos, ang)
+	ent:Spawn()
+	return ent
+end
+
+slib.SetComponent('FakePlayer', Class)
