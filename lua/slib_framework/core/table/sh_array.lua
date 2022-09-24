31047f2d54d65ec3da739b14bdbb29787502976a
Shark-vil <shark_vil@mail.ru>
2022-09-24 19:06:08 +0300

    Check valid type for "table.isArray"

--- lua/slib_framework/core/table/sh_array.lua
+++ lua/slib_framework/core/table/sh_array.lua
@@ -4,9 +4,12 @@ local math_random = math.random
 local table_remove = table.remove
 local table_Random = table.Random
 local table_insert = table.insert
+local type = type
+local table_type_name = 'table'
 --
 
 function table.isArray(t)
+	if type(t) ~= table_type_name then return false end
 	return #t > 0 and next(t, #t) == nil
 end
 
