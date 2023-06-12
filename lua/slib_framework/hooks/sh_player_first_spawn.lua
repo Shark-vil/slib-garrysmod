bd968e7583a8e7ccd4d25d4a892e157ca25efce8
Shark-vil <shark_vil@mail.ru>
2023-06-12 15:06:42 +0300

    Improved first spawn hook handler

--- lua/slib_framework/hooks/sh_player_first_spawn.lua
+++ lua/slib_framework/hooks/sh_player_first_spawn.lua
@@ -1,6 +1,6 @@
 if SERVER then
 	hook.Add('PlayerDisconnected', 'Slib_PlayerDisconnectedRemoveLoadedPlayer', function(disconnected_player)
-		if disconnected_player and IsValid(disconnected_player) then
+		if IsValid(disconnected_player) then
 			local hook_name = disconnected_player.slib_first_spawn_hook_name
 			if hook_name and isstring(hook_name) then
 				hook.Remove('SetupMove', hook_name)
@@ -25,50 +25,41 @@ if SERVER then
 		local hook_name = 'slib.FirstSpawnSetupMove_' .. slib.GenerateUid(ply:UserID())
 		ply.slib_first_spawn_hook_name = hook_name
 
-		hook.Add('SetupMove', hook_name, function(p, mv, cmd)
-			if p and ply and IsValid(ply) and IsValid(p) and p == ply and not cmd:IsForced() then
+		hook.Add('SetupMove', hook_name, function(move_player, move_data, move_command)
+			if not IsValid(ply) then
+				hook.Remove('SetupMove', hook_name)
+			elseif IsValid(move_player) and move_player == ply and not move_command:IsForced() then
+				hook.Remove('SetupMove', hook_name)
+
 				ply.snet_ready = true
 				ply.slib_first_spawn_hook_name = nil
 
 				table.insert(slib.Storage.LoadedPlayers, ply)
 
-				hook.Run('slib.FirstPlayerSpawn', ply)
-				snet.Request('slib_first_player_spawn', ply).InvokeAll()
+				local slib_hook = slib.Component('Hook')
+				slib_hook.SafeRun('slib.FirstPlayerSpawn', ply)
 
-				-- timer.Simple(1.5, function()
-				-- 	if not ply or not IsValid(ply) then return end
-				-- 	snet.Request('slib_first_player_spawn', ply).InvokeAll()
-				-- end)
-
-				hook.Remove('SetupMove', hook_name)
+				-- hook.Run('slib.FirstPlayerSpawn', ply)
+				snet.InvokeAll('slib_first_player_spawn', ply)
+				snet.Invoke('slib_first_player_spawn_sync', ply, slib.Storage.LoadedPlayers)
 			end
 		end)
 	end)
 
 	hook.Add('slib.FirstPlayerSpawn', 'Slib_SyncExistsNetworkVariable', function(ply)
 		for _, ent in ipairs(ents.GetAll()) do
-			if ent.slibVariables and table.Count(ent.slibVariables) ~= 0 then
-				for key, value in pairs(ent.slibVariables) do
-					if value ~= nil then
-						snet.Invoke('slib_entity_variable_set', ply, ent, key, value)
-					end
+			if not IsValid(ent) or not ent.slibVariables then continue end
+			for key, value in pairs(ent.slibVariables) do
+				if value ~= nil then
+					snet.Invoke('slib_entity_variable_set', ply, ent, key, value)
 				end
 			end
 		end
-
-		local players = {}
-
-		for _, aply in ipairs(player.GetAll()) do
-			if aply ~= ply and aply.snet_ready then
-				table.insert(players, aply)
-			end
-		end
-
-		snet.Invoke('slib_first_player_spawn_sync', ply, players)
 	end)
 else
 	snet.Callback('slib_first_player_spawn', function(_, ply)
 		slib.DebugLog('Player ', ply, ' first spawn on the server')
+
 		if ply == LocalPlayer() then
 			slib.DebugLog('You spawned on the server for the first time')
 		end
@@ -82,6 +73,9 @@ else
 	snet.Callback('slib_first_player_spawn_sync', function(_, players)
 		for i = 1, #players do
 			local ply = players[i]
+			if not IsValid(ply) or ply == LocalPlayer() or ply.snet_ready then
+				continue
+			end
 
 			ply.snet_ready = true
 			table.insert(slib.Storage.LoadedPlayers, ply)
