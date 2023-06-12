96bd976d67aeb8a7a7e58f7323cb44569fe514f8
Shark-vil <shark_vil@mail.ru>
2023-06-12 15:05:49 +0300

    Changed garbage collector of network messages

--- lua/slib_framework/network/base/sh_garbage_collection.lua
+++ lua/slib_framework/network/base/sh_garbage_collection.lua
@@ -3,8 +3,9 @@ local snet = slib.Components.Network
 local RealTime = RealTime
 local table_remove = table.remove
 local xpcall = xpcall
+local IsValid = IsValid
 local pairs = pairs
-local Warning = slib.Warning
+local Log = slib.Log
 local Error = slib.Error
 
 function snet.RunGarbageCollection(requests)
@@ -13,29 +14,29 @@ function snet.RunGarbageCollection(requests)
   for i = #requests, 1, -1 do
     local data = requests[i]
     if not data or (data.request and not data.request.eternal and data.timeout < RealTime()) then
-      if data and data.request and data.request.func_complete then
-        xpcall(function()
-          data.request.func_complete(data.receiver, data)
-        end, function(error_message)
-          Error('Failed to complete the request due to an error')
-          Error('NETWORK ERROR:\n' .. error_message)
-        end)
-      end
       table_remove(requests, i)
-    end
-
-    if data and data.request then
-      local request_name = data.request.name
-      counting_requests[request_name] = counting_requests[request_name] or 0
-      counting_requests[request_name] = counting_requests[request_name] + 1
+      if data and data.request then
+        if data.request.func_complete then
+          xpcall(function()
+            if not IsValid(data.receiver) or not data.receiver.IsBot or data.receiver:IsBot() then return end
+            data.request.func_complete(data.receiver, data)
+          end, function(error_message)
+            Error('Failed to complete the request due to an error')
+            Error('NETWORK ERROR:\n' .. error_message)
+          end)
+        end
+        local request_name = data.request.name
+        counting_requests[request_name] = counting_requests[request_name] or 0
+        counting_requests[request_name] = counting_requests[request_name] + 1
+      end
     end
   end
 
   local count = #requests
   if count >= 500 then
-    Warning('Something is making too many requests (' .. count .. ')')
+    Log('WARNING: Something is making too many requests (' .. count .. ')')
     for k, v in pairs(counting_requests) do
-      Warning('COUNTING REQUEST: ' .. k .. ' - ' .. v)
+      Log('COUNTING REQUEST: ' .. k .. ' - ' .. v)
     end
   end
 end
