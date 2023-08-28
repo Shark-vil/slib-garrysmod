6efaa55a96a40f546b8e9384c988ed0a3a134765
Shark-vil <shark_vil@mail.ru>
2023-08-28 12:44:51 +0300

    Update sh_time.lua

--- lua/slib_framework/extension/sh_time.lua
+++ lua/slib_framework/extension/sh_time.lua
@@ -1,16 +1,11 @@
 slib.deltaTime = slib.deltaTime or 0
-slib.fixedDeltaTime = slib.fixedDeltaTime or 0
+slib.fixedDeltaTime = slib.fixedDeltaTime or 0.02
 
 local SysTime = SysTime
-local FrameTime = FrameTime
 local start_delta_time = 0
 local is_second_tick = false
 
 hook.Add('Tick', 'Slib.Extension.Time.DeltaTime', function()
-	slib.fixedDeltaTime = FrameTime()
-end)
-
-hook.Add('Tick', 'Slib.Extension.Time.DixedDeltaTime', function()
 	if is_second_tick then
 		slib.deltaTime = SysTime() - start_delta_time
 	else
